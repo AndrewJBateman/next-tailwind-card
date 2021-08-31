0380a88cfb728f80b6f5c13dec438ddad6f1030f
AndrewJBateman <gomezbateman@yahoo.com>
2021-08-31 21:43:36 +0200

    chore: usused code removed

--- pages/_document.js
+++ pages/_document.js
@@ -14,24 +14,8 @@ class MyDocument extends Document {
 						name="description"
 						content="Build a Next.js PWA with 100% lighthouse score"
 					></meta>
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
 					<meta name="theme-color" content="#0D47A1" />
 					<link rel="manifest" href="/manifest.json" />
-					<link
-						href="/favicon-16x16.png"
-						rel="icon"
-						type="image/png"
-						sizes="16x16"
-					/>
-					<link
-						href="/favicon-32x32.png"
-						rel="icon"
-						type="image/png"
-						sizes="32x32"
-					/>
 					<link rel="apple-touch-icon" href="/apple-icon.png"></link>
 				</Head>
 				<body>
